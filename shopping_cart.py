bad5bfd03b820f4040a55c980140d23142b94b5e
atrox3d <atrox3d@gmail.com>
2024-07-06 11:00:47 +0200

    first test

---
+++ shopping_cart.py
@@ -0,0 +1,16 @@
+class ShoppingCart:
+
+    def __init__(self) -> None:
+        self.items: list[str] = []
+
+    def add(self, item:str):
+        self.items.append(item)
+
+    def size(self) -> int:
+        return len(self.items)
+    
+    def get_items(self) -> list[str]:
+        pass
+
+    def get_total_price(self, pricemap) -> int:
+        pass
